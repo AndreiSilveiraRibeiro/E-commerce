9d8156d005d1fc66f70115faac4f3d4fdc568722
Andreisr <pidecoribeiro2007@gmail.com>
2026-04-17 14:45:11 -0300

    Limpeza feita com sucesso!

---
+++ Analise/Analise de dados.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+
+info_pedidos = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\FACT_Orders.csv")
+produtos_vendidos = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Products.csv")
+info_clientes = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Customer.csv")
+info_entrega = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Delivery.csv")
+
+#Analisar primeiro as informações dos pedidos para ver se tem outliers
+print(info_pedidos.describe().loc[['mean', '25%', '50%', '75%', 'max']])
+
+Q1 = info_pedidos['Total'].quantile(0.25)
+Q3 = info_pedidos['Total'].quantile(0.75)
+IQR = Q3 - Q1
+
+limite_superior = Q3 + (1.5 * Q1)
+
+pedidos_normais = info_pedidos[info_pedidos['Total'] <= limite_superior]
+
+pedidos_vip = info_pedidos[info_pedidos['Total'] > limite_superior]
+
+
+print(f"Limite superior: R$ {limite_superior:.2f}")
+print(f"Quantidade de pedidos comuns: {len(pedidos_normais)}")
+print(f"Quantidade de pedidos comuns: {len(pedidos_vip)}")
--- Analise/Limpeza.py
+++ Analise/Limpeza.py
@@ -1,10 +1,20 @@
 import pandas as pd
+import numpy as np
 
-produtos = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Products.csv")
 info_pedidos = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\FACT_Orders.csv")
 produtos_vendidos = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Products.csv")
 info_clientes = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Customer.csv")
 info_entrega = pd.read_csv(r"C:\Users\andre\OneDrive\Documents\Repositorios\aprendizagem\EBAC\python\Projetos\E-commerce\Dados\DIM_Delivery.csv")
 
+#Analisando se tem dados nulos e duplicados
+print(f'Dados nulos nas informações dos produtos: \n{info_pedidos.isnull().sum()}')
+print(f'Dados duplicados nas informações dos produtos: \n{info_pedidos.duplicated().sum()}')
 
-print(info_clientes.isnull())
+print(f'Dados nulos nas informações dos clientes: \n{info_clientes.isnull().sum()}')
+print(f'Dados duplicados nas informações dos clientes: \n{info_clientes.duplicated().sum()}')
+
+print(f'Dados nulos nas informações das entregas: \n{info_entrega.isnull().sum()}')
+print(f'Dados nulos nas informações das entregas: \n{info_entrega.duplicated().sum()}')
+
+print(f'Dados nulos nas informações dos produtos vendidos: \n{produtos_vendidos.isnull().sum()}')
+print(f'Dados nulos nas informações dos produtos vendidos: \n{produtos_vendidos.duplicated().sum()}')
